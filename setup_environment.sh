ab6a86fd3eabc5d0ad3deb96b3ad0e2060a0658b
root <root@vps24476.publiccloud.com.br>
2020-11-28 19:12:44 -0300

    up

--- setup_environment.sh
+++ setup_environment.sh
@@ -10,9 +10,18 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 cp vimrclite ~/.vimrc
 cp tmuxconflite ~/.tmux.conf
 mkdir ~/.vim/colors
-cp colors/oceanic_next.vim ~/.vim/colors/oceanic_next.vim
+cp colors/elixir_box.vim ~/.vim/colors/elixir_box.vim
+cp colors/ruby_box.vim ~/.vim/colors/ruby_box.vim
+cp colors/rust_box.vim ~/.vim/colors/rust_box.vim
 cd ~/.vim
 git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
 
+echo "******************************************"
+echo " "
+echo " "
 echo "Setup done, open vim and run :PlugInstall"
+echo " "
+echo " "
+echo " "
+echo "******************************************"
 
